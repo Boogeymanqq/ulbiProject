ac3b57c145157ee442964d2dedfd90b026889abe
Anton.O <boogeymanqq@gmail.com>
2023-12-04 15:25:36 +0300

    fix

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -18,9 +20,15 @@ export default {
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
   moduleDirectories: ['node_modules'],
+  modulePaths: ['<rootDir>src'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  rootDir: '../../'
+  rootDir: '../../',
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
+  }
 
   // testMatch: '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
 
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import s from './Sidebar.module.scss';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
+import React from 'react';
 
 interface SidebarProps {
   className?: string;
@@ -15,8 +16,13 @@ export const Sidebar = ({ className }: SidebarProps) => {
     setCollapsed((prev) => !prev);
   };
   return (
-    <div className={classNames(s.Sidebar, { [s.collapsed]: collapsed }, [className])}>
-      <button onClick={onToggle}>toggle</button>
+    <div
+      data-testid='sidebar'
+      className={classNames(s.Sidebar, { [s.collapsed]: collapsed }, [className])}
+    >
+      <button data-testid='sidebar-toggle' onClick={onToggle}>
+        toggle
+      </button>
       <div className={s.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={s.lang} />
